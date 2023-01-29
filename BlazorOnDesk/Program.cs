dd8d68a6e2286f815e6fa7e9437c04be7c43aca8
NerveFalcon <suraevoleg@gmail.com>
2023-01-29 17:37:28 +0300

    Add BlazorOnDesk project

---
+++ BlazorOnDesk/Program.cs
@@ -0,0 +1,21 @@
+﻿using BlazorDesktop.Hosting;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Portfolio.UI;
+
+var builder = BlazorDesktopHostBuilder.CreateDefault( args );
+
+var connectionString = @"DataSource=""D:\Windows\Projects\Rider\Portfolio\src\Portfolio.UI.Server\app.db"";Cache=Shared";
+
+builder.UseWebViewInstaller();
+builder.Services.AddAuthorizationCore();
+builder.Services.AddPortfolio( options => options.UseSqlite( connectionString ) );
+
+builder.RootComponents.Add<App>( "#app" );
+builder.RootComponents.Add<HeadOutlet>( "head::after" );
+
+if( builder.HostEnvironment.IsDevelopment() ) { builder.UseDeveloperTools(); }
+
+await builder.Build()
+			 .RunAsync();
